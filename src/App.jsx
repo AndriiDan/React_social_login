da34b6a64ee491ab122ac9526aae2132c81c5d90
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-09-15 16:32:42 +0300

    Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar";
 import "./app.css";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div><Navbar /></div>;
+  return <div>
+    <Navbar />
+    <Home />
+    </div>;
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const posts = [
+    {
+        id: 1,
+        title: "5 Premium Theme for React",
+        img: "https://images.pexels.com/photos/7963572/pexels-photo-7963572.jpeg",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugiat voluptatem, id debitis consequatur necessitatibus quis provident, in odit eveniet quisquam assumenda reprehenderit, aspernatur minus.",
+        longDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, similique. Id optio ad nostrum impedit atque voluptatem, minus consequuntur molestiae tempora earum sequi sint, consequatur saepe asperiores, illum doloribus veniam consectetur hic unde? Odio rem consequuntur accusantium voluptatem ipsam voluptate laudantium repellat. Porro dolores commodi nobis, quas ullam dolorum dolor, ex culpa cupiditate obcaecati quis inventore praesentium at tenetur incidunt pariatur rem. Iste ex nostrum nesciunt delectus, sit exercitationem nemo. Non soluta modi veritatis velit nemo. Illum nobis, sunt assumenda labore a fugit alias fugiat vel odio! Magni sapiente doloribus excepturi illum ad vero in. Eaque quod eos atque ratione?"
+    },
+    {
+        id: 2,
+        title: "How to Animate Elements?",
+        img: "https://images.pexels.com/photos/1742370/pexels-photo-1742370.jpeg",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugiat voluptatem, id debitis consequatur necessitatibus quis provident, in odit eveniet quisquam assumenda reprehenderit, aspernatur minus.",
+        longDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, similique. Id optio ad nostrum impedit atque voluptatem, minus consequuntur molestiae tempora earum sequi sint, consequatur saepe asperiores, illum doloribus veniam consectetur hic unde? Odio rem consequuntur accusantium voluptatem ipsam voluptate laudantium repellat. Porro dolores commodi nobis, quas ullam dolorum dolor, ex culpa cupiditate obcaecati quis inventore praesentium at tenetur incidunt pariatur rem. Iste ex nostrum nesciunt delectus, sit exercitationem nemo. Non soluta modi veritatis velit nemo. Illum nobis, sunt assumenda labore a fugit alias fugiat vel odio! Magni sapiente doloribus excepturi illum ad vero in. Eaque quod eos atque ratione?"
+    },
+    {
+        id: 3,
+        title: "Learn JS in 30 days",
+        img: "https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugiat voluptatem, id debitis consequatur necessitatibus quis provident, in odit eveniet quisquam assumenda reprehenderit, aspernatur minus.",
+        longDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, similique. Id optio ad nostrum impedit atque voluptatem, minus consequuntur molestiae tempora earum sequi sint, consequatur saepe asperiores, illum doloribus veniam consectetur hic unde? Odio rem consequuntur accusantium voluptatem ipsam voluptate laudantium repellat. Porro dolores commodi nobis, quas ullam dolorum dolor, ex culpa cupiditate obcaecati quis inventore praesentium at tenetur incidunt pariatur rem. Iste ex nostrum nesciunt delectus, sit exercitationem nemo. Non soluta modi veritatis velit nemo. Illum nobis, sunt assumenda labore a fugit alias fugiat vel odio! Magni sapiente doloribus excepturi illum ad vero in. Eaque quod eos atque ratione?"
+    }
+];
