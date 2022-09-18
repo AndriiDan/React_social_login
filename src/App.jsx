8c5d4c8d3d8b98ab9b3660e9749f18ebd5ced140
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-09-18 22:56:07 +0300

    React-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import "./app.css";
 import Post from "./pages/Post";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    <Navbar />
-    {/* <Home /> */}
-    {/* <Post /> */}
-    <Login />
-  </div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import avatar from "../img/smile.jpg";
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <span className="logo">React App</span>
+            <span className="logo">
+                <Link className="link" to="/">React App</Link>
+            </span>
             <ul className="list">
                 <li className="listItem">
                     <img src={avatar} alt="" className="avatar" />
