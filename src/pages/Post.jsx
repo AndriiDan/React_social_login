b5201d3f8e769ff56c3ea1e338cef8921382d66c
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-09-18 23:35:08 +0300

    To switch automatically post/id.

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router";
 import { posts } from "../data";
 
 const Post = () => {
-    const post = posts[2];
+    const location = useLocation();
+
+    // при console.log(location) виведе об'єкт з параметрами pathname:"/post/1". Розбити по символу "/" і взяти значення індекса [2]
+    const path = location.pathname.split("/")[2];
+
+    // знайти id яке співпадає з індексом path
+    const post = posts.find((p) => p.id.toString() === path);
+
     return (
         <div className="post">
             <img src={post.img} alt="" className="postImg" />
