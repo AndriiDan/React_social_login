41c229aa19f2a7b9e38d02ef86b227aa9425a512
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-09-28 22:15:15 +0300

    github authorization (не працює).

--- backend/passport.js
+++ backend/passport.js
@@ -1,15 +1,29 @@
 // використити бібліотеку
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GithubStrategy = require('passport-github2').Strategy;
 const passport = require("passport");
 
 const GOOGLE_CLIENT_ID = "737459794686-is409vhe0v4qip7aog387j5crer27jvu.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "180994ee7d12d7aff368";
+GITHUB_CLIENT_SECRET = "6c1df27b3f2e74cb0e30a4edc9481c50f15a7de4"
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     // callbackURL: "http://localhost:5000/auth/google/callback"
     callbackURL: "/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        done(null, profile)
+    }
+));
+
+passport.use(new GithubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback"
 },
     function (accessToken, refreshToken, profile, done) {
         done(null, profile)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const passport = require("passport");
-const CLIENT_URL = "http://http://localhost:3000/";
+const CLIENT_URL = "http://localhost:3000/";
 
 router.get("/login/success", (req, res) => {
     if (req.user) {
@@ -9,9 +9,10 @@ router.get("/login/success", (req, res) => {
             message: "successfull",
             user: req.user,
             // cookies: req.cookies
-        })
+        });
     }
 });
+
 router.get("/login/failed", (req, res) => {
     res.status(401).json({
         success: false,
@@ -28,7 +29,14 @@ router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get("/google/callback", passport.authenticate("google", {
     successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed"
+    failureRedirect: "/login/failed",
+}));
+
+router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+
+router.get("/github/callback", passport.authenticate("github", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
 }));
 
 module.exports = router;
